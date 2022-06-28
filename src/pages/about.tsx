ca132fa45dc37f104d347f37e52241a0f86039dd
deniz aksu <deniz.aksu@dijitalkurye.com.tr>
2022-06-28 10:38:23 +0300

    about experiances add experiances comp add experiance-item comp add about update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import AboutExperiances from 'src/components/about-experiances'
 
 const AboutPage = () => {
-  return <div>AboutPage</div>
+  return <>
+    <h3 className="underline underline-offset-4 pt-3">Hakkımda</h3>
+    <p>
+      Meslek lisesinden mezun olmadan staj yaparak yazılım geliştirmeye başladım. Bu süreçte Full-Stack olarak görev aldım. Sonrasında üniversiteye gidip aynı dönemde çalışma hayatında bulunmaya devam ettim. Genel olarak Start-Up firmalarında yer aldım. Üniversiteden sonra kariyer hayatıma Front-End Developer olarak devam ediyorum.
+    </p>
+    <h4 className="underline underline-offset-3">Deneyim</h4>
+    <AboutExperiances />
+  </>
 }
 
 export default AboutPage
