bb11756b60ae00174d195955c9298c70977867dc
deniz aksu <deniz.aksu@dijitalkurye.com.tr>
2022-06-28 11:24:54 +0300

    project refactoring

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import AboutExperiances from 'src/components/about-experiances'
 
+const SKILLS = [
+  'Angular 8 - 10 - 11',
+  'Angular.js',
+  'React',
+  'Vue',
+  'JQuery',
+  'Electron',
+  'RxJs',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'Go',
+]
+
 const AboutPage = () => {
   return <>
     <h3 className="underline underline-offset-4 pt-3">Hakkımda</h3>
@@ -9,6 +24,14 @@ const AboutPage = () => {
     </p>
     <h4 className="underline underline-offset-3">Deneyim</h4>
     <AboutExperiances />
+    <h4 className='underline underline-offset-3 pt-3'>Yetenekler</h4>
+    <div className='grid grid-cols-4 gap-4'>
+      {
+        SKILLS.map(item => (
+          <span key={item}>{item}</span>
+        ))
+      }
+    </div>
   </>
 }
 
