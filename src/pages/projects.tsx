0f078ccaa390216c4adbc5484053f0bc0ed5491b
denizyoldas <daksu30@gmail.com>
2022-07-01 12:54:03 +0300

    project bug fix

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -5,7 +5,8 @@ import { IProject } from 'src/types/project'
 const PROJECTS: IProject[] = [
   {
     name: 'Wordonce Game',
-    description: 'Vue kullanarak freelance olarak geliştirdiğim bir kelime oyunu. Kısaca satırda veya sütünda aynı harf bulunmaması ve okunduğunda anlamlı bir cümle oluşması kurallarına dayanan bir oyun.',
+    description:
+      'Vue kullanarak freelance olarak geliştirdiğim bir kelime oyunu. Kısaca satırda veya sütünda aynı harf bulunmaması ve okunduğunda anlamlı bir cümle oluşması kurallarına dayanan bir oyun.',
     url: 'https://wordonce.com/',
     image: '/wordonce.png',
     technologies: ['Vue', 'TypeScript', 'Pinia', 'Vite', 'sass'],
@@ -13,15 +14,17 @@ const PROJECTS: IProject[] = [
   },
   {
     name: 'Portfolio',
-    description: 'React js kullanarak youtube da gördüklerime ekleyerek oluşturdum eski portfolio sitem.',
-    url: 'portfolio-denizyoldas.vercel.app',
+    description:
+      'React js kullanarak youtube da gördüklerime ekleyerek oluşturdum eski portfolio sitem.',
+    url: 'https://portfolio-denizyoldas.vercel.app',
     image: '/portfolio.png',
     technologies: ['React', 'sass', 'css'],
     createdAt: new Date('2021-10-27')
   },
   {
     name: 'Hotel App',
-    description: 'Bir case olarak gelen ve yaparken keyif aldığım. Kendim için geliştirdiğim bir otel puanlama ekleme uygulaması',
+    description:
+      'Bir case olarak gelen ve yaparken keyif aldığım. Kendim için geliştirdiğim bir otel puanlama ekleme uygulaması',
     url: 'https://github.com/denizyoldas/hotel-app',
     image: '/hotel-app.png',
     technologies: ['React', 'TypeScript', 'Tailwindcss'],
@@ -29,7 +32,8 @@ const PROJECTS: IProject[] = [
   },
   {
     name: 'Screen Record App',
-    description: 'Electron js kullanarak geliştirdiğim ekran kayıt uygulaması. Ekranın sesini ve bilgisayarın mikrofonundan gelen sesi kayıt altına alıp belirlenen dosyaya mp4 uzantılı bir çıktı alarak izlemenize olanak sağlıyor.',
+    description:
+      'Electron js kullanarak geliştirdiğim ekran kayıt uygulaması. Ekranın sesini ve bilgisayarın mikrofonundan gelen sesi kayıt altına alıp belirlenen dosyaya mp4 uzantılı bir çıktı alarak izlemenize olanak sağlıyor.',
     url: '#',
     image: '/no-image.webp',
     technologies: ['Electron', 'Bulma', 'JavaScript'],
@@ -37,7 +41,8 @@ const PROJECTS: IProject[] = [
   },
   {
     name: 'ToDo App',
-    description: 'React Native kullanarak geliştirdiğim bir not tutma uygulaması. Genel olarak ios uyumlu bir app olarak basit düzeyde not tutmaya yardımcı bir uygulama',
+    description:
+      'React Native kullanarak geliştirdiğim bir not tutma uygulaması. Genel olarak ios uyumlu bir app olarak basit düzeyde not tutmaya yardımcı bir uygulama',
     url: 'https://github.com/denizyoldas/todo-app',
     image: '/no-image.webp',
     technologies: ['React Native', 'Chakra UI', 'TypeScript', 'Jotai'],
