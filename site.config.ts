5effafcbeb9b4408a9436583c1c9fe65b6e93559
deniz aksu <deniz.aksu@dijitalkurye.com.tr>
2022-07-04 11:19:57 +0300

    google analytics add

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ export const meta = {
   title: "Deniz Aksu",
   description: "Developer",
   url: "http://denizaksu.com.tr",
-  ga: "",
+  ga: "G-2NGWQDGHCT",
   author: {
     email: "daksu30@gmail.com",
     name: "Deniz Aksu",
