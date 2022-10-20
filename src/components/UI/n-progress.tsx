65a7e0991da4c60bd1796e51dbce4d1c0ed7810d
denizyoldas <daksu30@gmail.com>
2022-10-21 00:56:19 +0300

    nextJS NProgress add

---
+++ src/components/UI/n-progress.tsx
@@ -0,0 +1,20 @@
+import { useTheme } from 'next-themes'
+import NextNProgress from 'nextjs-progressbar'
+import { useMemo } from 'react'
+
+const NProgress = () => {
+  const { theme } = useTheme()
+
+  const NProgressColor = useMemo(() => {
+    console.log(theme)
+    if (theme === 'dark') {
+      return '#facc14'
+    } else {
+      return '#c084fc'
+    }
+  }, [theme])
+
+  return <NextNProgress color={NProgressColor} />
+}
+
+export default NProgress
