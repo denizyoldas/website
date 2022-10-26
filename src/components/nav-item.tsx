b6a91e835e0f8e15430e2fea6485d681da07e6f2
denizyoldas <daksu30@gmail.com>
2022-10-26 13:33:15 +0300

    snapshot update nextjs new verison update

--- src/components/nav-item.tsx
+++ src/components/nav-item.tsx
@@ -7,17 +7,16 @@ function NavItem({ href, children }) {
   const isActive = href === pathname
 
   return (
-    <Link href={href}>
-      <a
-        className={cx(
-          'px-2 py-1 dark:text-white hover:underline hover:underline-offset-2',
-          !isActive && 'no-underline',
-          isActive &&
-            'text-gray-900 dark:text-yellow-500 underline-offset-1 font-bold'
-        )}
-      >
-        {children}
-      </a>
+    <Link
+      href={href}
+      className={cx(
+        'px-2 py-1 dark:text-white hover:underline hover:underline-offset-2',
+        !isActive && 'no-underline',
+        isActive &&
+          'text-gray-900 dark:text-yellow-500 underline-offset-1 font-bold'
+      )}
+    >
+      {children}
     </Link>
   )
 }
