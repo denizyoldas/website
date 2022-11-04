97c3a24f82d7e3947ee00710c0360d38cdbd7f7c
denizyoldas <daksu30@gmail.com>
2022-11-04 20:20:50 +0300

    new project

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,19 +1,31 @@
 import Head from 'next/head'
-import React from 'react'
+import { nanoid } from 'nanoid'
 import ProjectItem from 'src/components/project-item'
 import { IProject } from 'src/types/project'
 
 const PROJECTS: IProject[] = [
   {
-    name: 'Wordonce Game',
-    description:
-      'Vue kullanarak freelance olarak geliştirdiğim bir kelime oyunu. Kısaca satırda veya sütünda aynı harf bulunmaması ve okunduğunda anlamlı bir cümle oluşması kurallarına dayanan bir oyun.',
-    url: 'https://wordonce.com/',
-    image: '/wordonce.png',
-    technologies: ['Vue', 'TypeScript', 'Pinia', 'Vite', 'sass'],
-    createdAt: new Date('2022-04-20')
+    id: nanoid(),
+    name: 'siparislio.com',
+    description: `
+    Siparlio e-ticaret sitesinde Next.JS kullanarak
+    geliştirmeler yaptım. Bu projede Next.JS, Redux, 
+    react-hook-form, gibi teknolojileri kullandım. Ödeme entegrasyonu, admin paneli yeni featurelar eklenmesi,
+    ürünlerin listelenmesi, filtrelenmesi, aranması gibi işlemleri yaptım. Bunun yanı sıra vercel config ve seri deployment ayarları yaptım.`,
+    url: 'https://siparislio.com/',
+    image: '/siparislio.png',
+    technologies: [
+      'Next.js',
+      'TypeScript',
+      'sass',
+      'tailwindcss',
+      'react-hook-form',
+      'redux'
+    ],
+    createdAt: new Date('2022-09-16')
   },
   {
+    id: nanoid(),
     name: 'Mai Organizasyon',
     description:
       'Mai Organizasyon web sitesi tasarımı, kodlanması ve google işletmem hesabı kurulumu.',
@@ -30,6 +42,7 @@ const PROJECTS: IProject[] = [
     createdAt: new Date('2022-08-04')
   },
   {
+    id: nanoid(),
     name: 'Digital Agancy',
     description: 'Digital Agancy web sitesi tasarımı.',
     url: 'https://digital-website-kappa.vercel.app/',
@@ -45,6 +58,17 @@ const PROJECTS: IProject[] = [
     createdAt: new Date('2022-10-30')
   },
   {
+    id: nanoid(),
+    name: 'Wordonce Game',
+    description:
+      'Vue kullanarak freelance olarak geliştirdiğim bir kelime oyunu. Kısaca satırda veya sütünda aynı harf bulunmaması ve okunduğunda anlamlı bir cümle oluşması kurallarına dayanan bir oyun.',
+    url: 'https://wordonce.com/',
+    image: '/wordonce.png',
+    technologies: ['Vue', 'TypeScript', 'Pinia', 'Vite', 'sass'],
+    createdAt: new Date('2022-04-20')
+  },
+  {
+    id: nanoid(),
     name: 'Google Chrome Bookmarks Extension',
     description: 'Google Chrome Bookmarks Extension',
     url: 'https://google.com/chrome-bookmarks-extension',
@@ -53,6 +77,7 @@ const PROJECTS: IProject[] = [
     createdAt: new Date('2022-08-27')
   },
   {
+    id: nanoid(),
     name: 'Portfolio',
     description:
       'React js kullanarak youtube da gördüklerime ekleyerek oluşturdum eski portfolio sitem.',
@@ -62,6 +87,7 @@ const PROJECTS: IProject[] = [
     createdAt: new Date('2021-10-27')
   },
   {
+    id: nanoid(),
     name: 'Hotel App',
     description:
       'Bir case olarak gelen ve yaparken keyif aldığım. Kendim için geliştirdiğim bir otel puanlama ekleme uygulaması',
@@ -71,6 +97,7 @@ const PROJECTS: IProject[] = [
     createdAt: new Date('2022-06-20')
   },
   {
+    id: nanoid(),
     name: 'Screen Record App',
     description:
       'Electron js kullanarak geliştirdiğim ekran kayıt uygulaması. Ekranın sesini ve bilgisayarın mikrofonundan gelen sesi kayıt altına alıp belirlenen dosyaya mp4 uzantılı bir çıktı alarak izlemenize olanak sağlıyor.',
@@ -80,6 +107,7 @@ const PROJECTS: IProject[] = [
     createdAt: new Date('2022-04-24')
   },
   {
+    id: nanoid(),
     name: 'ToDo App',
     description:
       'React Native kullanarak geliştirdiğim bir not tutma uygulaması. Genel olarak ios uyumlu bir app olarak basit düzeyde not tutmaya yardımcı bir uygulama',
@@ -100,7 +128,7 @@ const Projects = () => {
       <h3 className="underline underline-offset-4 pt-3">Projeler</h3>
       <ul className="grid grid-flow-row gap-10 place-items-center">
         {PROJECTS.map(project => (
-          <ProjectItem key={project.name} {...project} />
+          <ProjectItem key={project.id} {...project} />
         ))}
       </ul>
     </>
