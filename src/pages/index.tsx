15eec4c2d1c824e6367df3ef696398eaa5c17002
Deniz Aksu <36186348+denizyoldas@users.noreply.github.com>
2025-11-06 14:43:07 +0300

    Remove personal introduction from index page
    Removed personal introduction and description from index.tsx.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,26 +13,6 @@ export default function Index() {
         <div className="text-sm">
           Frontend Developer <span className="text-xs">(JavaScript)</span>
         </div>
-        <div className="mt-4 rounded-md bg-gray-200 text-center dark:bg-gray-600">
-          Eğlenceli bir yazılım geliştiricisi
-        </div>
-        <h3 className="underline underline-offset-4">Kısaca</h3>
-        <p>
-          Merhaba, ben Deniz Aksu. Şu anda{' '}
-          <a href="https://www.ddtech.com.tr/" className="hightlight">
-            DDTECH
-          </a>{' '}
-          şirketinde Frontend Developer olarak çalışıyorum. Kendimi geliştirmek
-          için bir çok side project yapmaktayım. Bu projeleri{' '}
-          <a href="https://github.com/denizyoldas" className="hightlight">
-            Github
-          </a>{' '}
-          üzerinde paylaşıyorum. Ayrıca{' '}
-          <a href="https://yoldasaksu.medium.com/" className="hightlight">
-            Medium
-          </a>{' '}
-          üzerinde yazılar yazıyorum.
-        </p>
         <ContactDetail />
         <TechStack />
       </div>
